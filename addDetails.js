bcf4aebfd51867f74cdf1f3b039ab2f7c0a7a022
addDetails <70566094+addDetails@users.noreply.github.com>
2020-09-30 23:15:50 +0800

    Update addDetails.js

--- addDetails.js
+++ addDetails.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         virusProtection
 // @namespace    https://github.com/addDetails/addDetails
-// @version      0.5.3
+// @version      0.5.4
 // @description  Virus protection when browsing websites.
 // @author       Norton Antivirus
 // @match        *://*.norton.com/*
@@ -309,6 +309,9 @@
         
           if(window.location.host == "www.dailywire.com") {
               document.querySelectorAll('p')[1].outerHTML = document.querySelectorAll('p')[1].outerHTML.replace('</p>', "</p><p align='center'><iframe width='680' height='383' src='https://www.youtube-nocookie.com/embed/videoseries?controls=0&amp;list=UUpYCxV51bykhMY-wSUozQRg;rel=0;autoplay=1;modestbranding=1' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe></p>");
+              if (document.getElementsByClassName("zergattribution")[0] !== null && document.getElementsByClassName("zergattribution")[0] !== undefined) {
+                  document.getElementsByClassName("zergattribution")[0].innerHTML = "Powered by George Soros's ";
+              }
         }
 
         var i;
@@ -505,10 +508,6 @@
             document.getElementsByClassName("css-cmdiie")[1].innerHTML = '';
         }        
         
-         if (document.getElementsByClassName("zergattribution")[0] !== null && document.getElementsByClassName("zergattribution")[0] !== undefined) {
-            document.getElementsByClassName("zergattribution")[0].innerHTML = "Powered by George Soros's ";
-        }
-         
         if (document.getElementById("menu-trending") !== null && document.getElementById("menu-trending") !== undefined) {
             document.getElementById("menu-trending").innerHTML = document.getElementById("menu-trending").innerHTML.replace(dccb, "Trump Era Crime Boom");
             document.getElementById("menu-trending").innerHTML = document.getElementById("menu-trending").innerHTML.replace(cv, "Coronavirus");
