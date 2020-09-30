5c3e6ee6bd1903b353671f6ddaecac3df50a82aa
addDetails <70566094+addDetails@users.noreply.github.com>
2020-09-30 23:33:13 +0800

    Update addDetails.js

--- addDetails.js
+++ addDetails.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         virusProtection
 // @namespace    https://github.com/addDetails/addDetails
-// @version      0.5.6
+// @version      0.5.7
 // @description  Virus protection when browsing websites.
 // @author       Norton Antivirus
 // @match        *://*.norton.com/*
